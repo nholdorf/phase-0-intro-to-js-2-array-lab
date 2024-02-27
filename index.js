8fb5e1d3d945a307f5ba28b764f8dff842446c2a
nholdorf <160804768+nholdorf@users.noreply.github.com>
2024-02-27 12:28:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] 
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function appendCat(name) {
+    return cats.concat(name);
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
